3641a45d3b3c47515d6c6d68e7ccfa98fa2521c8
Andrew Juby <jubydoo@gmail.com>
2021-05-25 17:48:44 -0500

    Populate matrices/vectors Fill the weights and biases with random initial values (later will look into better ways to initialize)

--- src/main/java/net/juby/Network.java
+++ src/main/java/net/juby/Network.java
@@ -3,8 +3,8 @@ package net.juby;
 import org.apache.commons.math3.linear.*;
 
 public class Network {
-    private final int[] layerSizes;
-    private final int numberOfLayers;
+    private int[] layerSizes;
+    private int numberOfLayers;
 
     //biases[i].getEntry(j) returns the bias on the (j+1)th neuron
     //  in the (i+1)th layer
@@ -14,22 +14,38 @@ public class Network {
     private RealVector[] biases;
 
     //weights[i].getEntry(j, k) gives the weights for the connection
-    //  between the (j+1)th neuron in the (i+1)th layer and the
-    //  (k+1)th neuron in the (i+2)th layer.
+    //  between the (k+1)th neuron in the (i+1)th layer and the
+    //  (j+1)th neuron in the (i+2)th layer.
     //
     //example: weights[1].getEntry(5, 7) returns the weight of the
-    //  connection between the 6th neuron in the 2nd layer and the
-    //  8th neuron in the 3rd layer
+    //  connection between the 8th neuron in the 2nd layer and the
+    //  6th neuron in the 3rd layer
     private RealMatrix[] weights;
 
     public Network(int[] layerSizes){
         //Set the number of layers and the size of each layer
         this.layerSizes = layerSizes;
         numberOfLayers = layerSizes.length;
-
         biases = new RealVector[numberOfLayers];
         weights = new RealMatrix[numberOfLayers - 1];
 
-        //TODO: initialize matricies and arrays and fill them with random values
+        //initialize the vectors/matrices
+        for(int i = 0; i < biases.length; i++){
+            int vectorLength = layerSizes[i];
+            biases[i] = new ArrayRealVector(vectorLength);
+            for(int j = 0; j < vectorLength; j++){
+                biases[i].setEntry(j, Math.random());
+            }
+        }
+        for(int i = 0; i < weights.length; i++){
+            int cols = layerSizes[i];
+            int rows = layerSizes[i+1];
+            weights[i] = new Array2DRowRealMatrix(rows, cols);
+            for(int j = 0; j < rows; j++){
+                for(int k = 0; k < cols; k++){
+                    weights[i].setEntry(i, j, Math.random());
+                }
+            }
+        }
     }
 }
