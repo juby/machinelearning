da1688504e376e7d266e9bd1bc854d5e4832a7e1
Andrew Juby <jubydoo@gmail.com>
2021-06-09 18:13:23 -0500

    Fixed the biases array Including the input layer in the biases array would have made the math later more complicated. Removing it simplifies things.

--- src/main/java/net/juby/Network.java
+++ src/main/java/net/juby/Network.java
@@ -8,42 +8,21 @@ import net.juby.mnist.MnistReader;
 
 
 public class Network {
-    //array of the number of neurons in each layer
     private int[] layerSizes;
-
-    //number of layers in the network, equivalent to layerSizes.length
     private int numberOfLayers;
-
-    //biases[i].getEntry(j) returns the bias on the (j+1)th neuron
-    //    in the (i+1)th layer.
-    //
-    //example: biases[0].getEntry(1) returns the bias on the 2nd
-    //  neuron in the 1st layer.
     private RealVector[] biases;
-
-    // weights[i].getEntry(j, k) gives the weights for the connection
-    //    between the (k+1)th neuron in the (i+1)th layer and the
-    //    (j+1)th neuron in the (i+2)th layer.
-    //
-    // example: weights[1].getEntry(5, 7) returns the weight of the
-    //    connection between the 8th neuron in the 2nd layer and the
-    //    6th neuron in the 3rd layer.
     private RealMatrix[] weights;
 
     public Network(int[] layerSizes){
         //Set the number of layers and the size of each layer.
         this.layerSizes = layerSizes;
         numberOfLayers = layerSizes.length;
-        biases = new RealVector[numberOfLayers];
+        biases = new RealVector[numberOfLayers - 1];
         weights = new RealMatrix[numberOfLayers - 1];
 
         // Initialize the weights and biases.
-        // First set all of the biases in the first layer to zero. The first
-        // layer is the input layer, so no biases are needed. By setting it to
-        // all zeros it makes our math a little easier later.
-        for(int i = 0; i < layerSizes[0]; i++) biases[0].setEntry(i, 0.0);
 
-        // Then create the vectors for each layer and initialize with random values.
+        // Create the vectors for each layer and initialize with random values.
         for(int i = 1; i < biases.length; i++){
             int vectorLength = layerSizes[i];
             biases[i] = new ArrayRealVector(vectorLength);
