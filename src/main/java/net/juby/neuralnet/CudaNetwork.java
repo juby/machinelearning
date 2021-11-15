9c6ca46a25c3367bd0f01aa7d5cf669c7ca40397
Andrew Juby <jubydoo@gmail.com>
2021-11-15 11:09:58 -0600

    CudaNetwork convertData
    Convert the 2D arrays of int grayscale values to 1D arrays of values normalized between 0 and 1.

--- src/main/java/net/juby/neuralnet/CudaNetwork.java
+++ src/main/java/net/juby/neuralnet/CudaNetwork.java
@@ -1,7 +1,12 @@
 package net.juby.neuralnet;
 
 import jcuda.*;
+import net.juby.neuralnet.mnist.MnistReader;
+import org.apache.commons.lang3.time.StopWatch;
+
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A neural network that trains and identifies handwritten digits using the
@@ -11,7 +16,8 @@ import java.util.Random;
  * <a href="https://developer.nvidia.com/cuda-toolkit>CUDA</a>.
  *
  * This first iteration will strictly use a quadratic cost function, additional
- * cost functions will be added later.
+ * cost functions will be added later. Also, no command line options are supported
+ * yet.
  *
  * @author Andrew Juby (jubydoo AT gmail DOT com)
  * @version 2.0 7/13/2021
@@ -62,16 +68,78 @@ public class CudaNetwork {
         }
     }
 
+    public static void main(String[] args){
+        // Training values setup.
+        int[] values = new int[]{784, 30, 10};
+        int epochs = 30;
+        int miniBatchSize = 10;
+        double eta = 3.0;
+
+        String dataFolder = "C:\\Users\\jubyd\\Projects\\machinelearning\\";
+        String trainingLabelsFileLocation = dataFolder + "mnist_data\\train-labels.idx1-ubyte";
+        String trainingDataFileLocation = dataFolder + "mnist_data\\train-images.idx3-ubyte";
+        String testLabelsFileLocation = dataFolder + "mnist_data\\t10k-labels.idx1-ubyte";
+        String testDataFileLocation = dataFolder + "mnist_data\\t10k-images.idx3-ubyte";
+
+        // Variable initialization.
+        int[] trainingLabels;
+        int[] testLabels;
+        List<int[][]> trainingData;
+        List<int[][]> testData;
+        StopWatch stopWatch = new StopWatch();
+        CudaNetwork network = new CudaNetwork(values);
+        double[][] trainingMatrix = null, testMatrix = null;
+
+        // Extract the MNIST data.
+        trainingLabels = MnistReader.getLabels(trainingLabelsFileLocation);
+        trainingData = MnistReader.getImages(trainingDataFileLocation);
+        testLabels = MnistReader.getLabels(testLabelsFileLocation);
+        testData = MnistReader.getImages(testDataFileLocation);
+
+        convertData(trainingData, testData, trainingMatrix, testMatrix);
+
+        stopWatch.start();
+        network.stochasticGradientDescent(trainingLabels, trainingMatrix,
+                testLabels, testMatrix, epochs, miniBatchSize, eta);
+        stopWatch.stop();
+        System.out.println("Network trained and tested in " +
+                stopWatch.getTime(TimeUnit.SECONDS) + " seconds.");
+    }
+
+    private static void convertData(List<int[][]> trainingData, List<int[][]> testData,
+                                    double[][] trainingMatrix, double[][] testMatrix){
+        trainingMatrix = new double[trainingData.get(0).length][trainingData.get(0)[0].length];
+        testMatrix = new double[testData.get(0).length][testData.get(0)[0].length];
+
+        // Flatten training image data, normalize, and convert to double values.
+        for (int[][] tempArray : trainingData) {
+            for (int j = 0; j < tempArray.length; j++) {
+                for (int k = 0; k < tempArray[j].length; k++) {
+                    trainingMatrix[j][k] = tempArray[j][k] / 255.0;
+                }
+            }
+        }
+
+        // Flatten test image data, normalize, and convert to double values.
+        for (int[][] tempArray : testData) {
+            for (int m = 0; m < tempArray.length; m++) {
+                for (int n = 0; n < tempArray[m].length; n++) {
+                    testMatrix[m][n] = tempArray[m][n] / 255.0;
+                }
+            }
+        }
+    }
+
     private double[] feedForward(double[] input){
         //TODO feedForward
         return input;
     }
 
-    private void stochasticGradientDescent(double[][] trainingMatrix,
-                                             double[][] testMatrix,
-                                             int epochs,
-                                             int miniBatchSize,
-                                             double eta){
+    private void stochasticGradientDescent(int[] trainingLabels, double[][] trainingMatrix,
+                                           int[] testLabels, double[][] testMatrix,
+                                           int epochs,
+                                           int miniBatchSize,
+                                           double eta){
         //TODO: SGD
     }
 
